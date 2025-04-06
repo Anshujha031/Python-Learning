0a70a7440e89aed6eb38373b4f79aabba6fe97ac
naveenjha031 <naveenjha344@gmail.com>
2025-04-06 23:24:23 +0530

    python

--- default_Argument.py
+++ default_Argument.py
@@ -3,12 +3,17 @@
 #                     make your functions more flexible, reduces no of arguments
 #                     1. positional, 2. DEFAULT, 3. keyword, 4. arbitary
 
-import time
 
-def count(end,start =0):
-    for x in range(start, end+1):
-        print(x)
-        time.sleep(1)
-    print("Done!")
+# import time
 
-count(10)
+# def count(end,start =0):
+#     for x in range(start, end+1):
+#         print(x)
+#         time.sleep(1)
+#     print("Done!")
+
+# count(10)
+
+
+#========================================================================
+
