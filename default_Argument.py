cc34a22b13ce9ef158ef4cc7aeacf76a382d4dd8
naveenjha031 <naveenjha344@gmail.com>
2025-04-06 23:24:00 +0530

    python

---
+++ default_Argument.py
@@ -0,0 +1,14 @@
+# default arguments = A default value for certain parameters
+#                     default is used when that argument is ommitted
+#                     make your functions more flexible, reduces no of arguments
+#                     1. positional, 2. DEFAULT, 3. keyword, 4. arbitary
+
+import time
+
+def count(end,start =0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("Done!")
+
+count(10)
